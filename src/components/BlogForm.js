d5cbd4042d81d348e8851ce4a8526d150d65da8c
Ross Middleton <5366498+R-Middleton@users.noreply.github.com>
2022-02-14 23:39:59 +0000

    Added Blog Form Functionality

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BlogService from '../services/blogs'
 
 const BlogForm = () => {
-  const addBlog = () => {}
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  const addBlog = (event) => {
+    event.preventDefault()
+
+    const newBlog = {
+      title: title,
+      author: author,
+      url: url,
+    }
+
+    BlogService.create(newBlog)
+
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+  }
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value)
+  }
+
+  const handleAuthorChange = (event) => {
+    setAuthor(event.target.value)
+  }
+
+  const handleUrlChange = (event) => {
+    setUrl(event.target.value)
+  }
+
   return (
     <div>
       <h2>Create New</h2>
@@ -10,18 +41,18 @@ const BlogForm = () => {
         <div>
           {' '}
           Title:
-          <input type='test' />
+          <input name='title' value={title} onChange={handleTitleChange} />
         </div>
         <div>
           Author:
-          <input type='text' />
+          <input name='author' value={author} onChange={handleAuthorChange} />
         </div>
         <div>
           URL:
-          <input type='text' />
+          <input name='url' value={url} onChange={handleUrlChange} />
         </div>
         <div>
-          <button>Save</button>
+          <button type='submit'>Save</button>
         </div>
       </form>
     </div>
